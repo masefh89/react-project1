fe81b1ea981fa1e9bf057ce1b67bd0614e9236d7
Mohammed Asef Hashemi <masefh.fi@gmail.com>
2026-02-04 11:54:06 +0200

    "Update Vite base path for GitHub Pages"

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/react-project1/',
   plugins: [react()],
-  base: '/react-project1/'
 })
